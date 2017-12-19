e2d93427b26b56ca9c42a337e0d510f089949307
wanzailin <1058794886@qq.com>
2017-12-19 23:28:49 +0800

    2017-12-19

---
+++ src/main/java/sprider/read/douban/com/service/pipeline/EBookPipeline.java
@@ -0,0 +1,12 @@
+package sprider.read.douban.com.service.pipeline;
+
+import us.codecraft.webmagic.ResultItems;
+import us.codecraft.webmagic.Task;
+import us.codecraft.webmagic.pipeline.Pipeline;
+
+public class EBookPipeline implements Pipeline
+{
+    public void process(ResultItems resultItems, Task task) {
+
+    }
+}
